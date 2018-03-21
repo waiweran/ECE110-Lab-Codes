ca31a1011b2451f69d5d5418f61acfcd47f84a4c
Nathaniel Brooke <neb17@duke.edu>
2018-03-21 19:10:32 -0400

    Updated Compass Test

--- Compass_Test/Compass_Test.ino
+++ Compass_Test/Compass_Test.ino
@@ -1,36 +1,29 @@
-#include <Wire.h>
+#include <Wire.h> // Required for Compass Sensor
 
-#define Addr 0x1E               // 7-bit address of HMC5883 compass
+/*
+ * HMC5883 Compass Sensor Test Code
+ * Written by Nathaniel Brooke
+ * Adapted from Parallax
+ * 
+ * LCD Connection:
+ * RX to TX3 (pin 14)
+ */
+
+// 7-bit address of HMC5883 compass, change if using multiple
+#define Addr 0x1E
 
 void setup() {
-  Serial1.begin(9600);
-  Serial.begin(9600);
-  delay(100);                   // Power up delay
-  Wire.begin();
-
-  // Set operating mode to continuous
-  Wire.beginTransmission(Addr);
-  Wire.write(byte(0x02));
-  Wire.write(byte(0x00));
-  Wire.endTransmission();
+  Serial.begin(9600); // Initialize Serial to computer
+  Serial3.begin(9600); // Initialize Serial to LCD
+  startCompass(); // Start the Compass Sensor
 }
 
 void loop() {
-  int x, y, z;
+  // Read Compass sensor
+  int x, y, z; // Variables to store sensor readings
+  readCompass(&x, &y, &z); // Read values from compass sensor
 
-  // Initiate communications with compass
-  Wire.beginTransmission(Addr);
-  Wire.write(byte(0x03));       // Send request to X MSB register
-  Wire.endTransmission();
-
-  Wire.requestFrom(Addr, 6);    // Request 6 bytes; 2 bytes per axis
-  if (Wire.available() <= 6) {  // If 6 bytes available
-    x = Wire.read() << 8 | Wire.read();
-    z = Wire.read() << 8 | Wire.read();
-    y = Wire.read() << 8 | Wire.read();
-  }
-
-  // Print raw values
+  // Print raw values to Serial Monitor
   Serial.print("X=");
   Serial.print(x);
   Serial.print(", Y=");
@@ -39,35 +32,54 @@ void loop() {
   Serial.print(z);
   Serial.print("\t\t");
 
-  Serial1.write(12);
+  // Print raw values to LCD
+  Serial1.write(12); // Clear LCD
   Serial1.print(x);
   Serial1.print(" ");
   Serial1.print(y);
   Serial1.print(" ");
   Serial1.print(z);
-  Serial1.write(13);
+  Serial1.write(13); // Write newline to LCD
 
+  // Check for magnet
   if(x < -1000 || x > 1000 || y < -1000 || y > 1000 || z < -1000 || z > 1000) {
     Serial1.print("Magnet Found");
     Serial.print("Magnet Found");
-  }
-  else if (x < -110 && x > -130 && y > 100) {
-    Serial1.print("North");
-    Serial.print("North");
-  }  
-  else if (x < -340 && x > -360) {
-    Serial1.print("East");
-    Serial.print("East");
-  }  
-  else if (x < -130 && x > -150 && y < 100) {
-    Serial1.print("South");
-    Serial.print("South");
-  }  
-  else if (x > 70 && x < 90) {
-    Serial1.print("West");
-    Serial.print("West");
   }
   Serial.println();
 
   delay(500);
 }
+
+/*
+ * Initializes Communication with Compass
+ */
+void startCompass() {
+  delay(100); // Power up delay
+  Wire.begin(); // Initialize I2C 
+
+  // Set operating mode to continuous
+  Wire.beginTransmission(Addr);
+  Wire.write(byte(0x02));
+  Wire.write(byte(0x00));
+  Wire.endTransmission();
+}
+
+/*
+ * Loads values read from compass sensor
+ * Magnetic field strength in X, Y, and Z
+ */
+void readCompass(int *x, int *y, int *z) {
+    // Initiate communications with compass
+  Wire.beginTransmission(Addr);
+  Wire.write(byte(0x03));       // Send request to X MSB register
+  Wire.endTransmission();
+
+  Wire.requestFrom(Addr, 6);    // Request 6 bytes; 2 bytes per axis
+  if (Wire.available() <= 6) {  // If 6 bytes available
+    *x = Wire.read() << 8 | Wire.read();
+    *z = Wire.read() << 8 | Wire.read();
+    *y = Wire.read() << 8 | Wire.read();
+  }
+}
+
